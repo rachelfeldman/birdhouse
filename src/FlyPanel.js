6b15187d0876c32af72de80c848941e5c5fc0eeb
Carlos Quinones <cquinones100@gmail.com>
2019-07-12 09:47:44 -0400

    add avatar

--- src/FlyPanel.js
+++ src/FlyPanel.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Panel from './Panel.js';
 import './FlyPanel.css'
+import avatar from './avatar.jpeg';
+
+const USER = {
+  avatar: avatar,
+  name: 'Carlos'
+};
 
 const FlyPanel = () => {
   return (
     <Panel fontAwesomeName="fas fa-feather-alt" title='fly' gridColumn='2'>
       <div className='flypanel'>
+        <div className='avatar'>
+          <img className='avatar' src={avatar} alt='avatar' />
+          <div className='avatar-name'>
+            {USER.name}
+          </div>
+        </div>
         <textarea className='flypanel' rows="10" />
         <button className='flypanel'>submit</button>
       </div>
